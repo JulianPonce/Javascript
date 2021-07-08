5ab1e9c305018b4587e6f81cebfd8eff7cb72b3a
Julian Ponce <juliangponce@gmail.com>
2021-07-08 13:44:25 -0300

    se agregaron img a cards

--- desafios/mayorprecio.js
+++ desafios/mayorprecio.js
@@ -28,12 +28,12 @@ function imprimirmayor() {
     listamayorprecio.forEach(e => {
         idImprimir.innerHTML += `
         <div class="card" style="width: 15rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="${e.img}" class="card-img-top" alt="...">
         <div class="card" style="width: 15rem;">
             
             <ul class="list-group list-group-flush">
-                <li class="list-group-item">${e.categoria}</li>
-                <li class="list-group-item">${e.marca}</li>
+                <li class="list-group-item">${e.genero}</li>
+                <li class="list-group-item">${e.banda}</li>
                 <li class="list-group-item">${e.precio}</li>
                 <a href="#" class="btn btn-dark" role="button" data-bs-toggle="button">Comprar</a>
             </ul>
